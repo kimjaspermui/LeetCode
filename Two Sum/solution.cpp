af7eaf26c6b214584383771860334d31ff7aaf76
Jasper Mui <kmui@ucsd.edu>
2017-07-10 12:18:41 -0700

    Second attempt using hash map, 9ms

--- Two Sum/solution.cpp
+++ Two Sum/solution.cpp
@@ -1,47 +1,30 @@
-#include <algorithm>
-
-int findNum(vector<int>& myVector, int number, int avoid) {
-
-    for (int i = 0; i < myVector.size(); i++) {
-
-        if (myVector[i] == number && i != avoid) {
-
-            return i;
-        }
-    }
-}
-
-void copyVector(vector<int>& vector1, vector<int>& vector2) {
-
-    for (int num : vector2) {
-
-        vector1.push_back(num);
-    }
-}
-
 class Solution {
 public:
 
     vector<int> twoSum(vector<int>& nums, int target) {
 
-        vector<int> sorted;
-        copyVector(sorted, nums);
-        std::sort(sorted.begin(), sorted.end());
         vector<int> result;
+        unordered_map<int, int> myMap;
 
-        for (int i = 0; i < sorted.size(); i++) {
+        for (int i = 0; i < nums.size(); i++) {
 
-            int pair = target - sorted[i];
+            int pair = target - nums[i];
+            auto pairIterator = myMap.find(pair);
 
-            if (binary_search(sorted.begin(), sorted.end(), pair)) {
+            if (pairIterator != myMap.end()) {
 
-                result.push_back(findNum(nums, sorted[i], -1));
-                result.push_back(findNum(nums, pair, result[0]));
+                result.push_back(pairIterator->second);
+                result.push_back(i);
 
                 return result;
+            }
+
+            else {
+
+                myMap.insert({nums[i], i});
             }
         }
 
         return result;
     }
-}
+};
