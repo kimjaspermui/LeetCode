1600613933dfb7cf34f59d6f5629d33718f4251f
Kim Jasper Mui <kmui@ucsd.edu>
2017-08-23 12:07:15 -0700

    Made the solution much more concise by using ternary operator

--- Add Two Numbers/solution.cpp
+++ Add Two Numbers/solution.cpp
@@ -9,66 +9,33 @@
 class Solution {
 public:
     ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-        // variables for summation
-        int num1 = 0;
-        int num2 = 0;
-        int sum = 0;
-        
-        // the carry from previous sum
-        int carry = 0;
-        
-        // get the sum
-        num1 = l1->val;
-        num2 = l2->val;
-        sum = num1 + num2 + carry;
-        carry = sum / 10;
-        
-        // new linkedlist to return
-        ListNode* out = new ListNode(sum % 10);
-        ListNode* result = out;
-        
-        // move pointers
-        l1 = l1->next;
-        l2 = l2->next;
 
-        // keep adding til both of them are NULL
-        while (l1 || l2) {
-            
-            num1 = 0;
-            num2 = 0;
-
-            // case for first linkedlist
-            if (l1) {
-                
-                num1 = l1->val;
-                l1 = l1->next;
-            }
-            
-            // case for second linkedlist
-            if (l2) {
-                
-                num2 = l2->val;
-                l2 = l2->next;
-            }
+        // ListNode, where result is head then myPointer will incrementally add new node
+        ListNode result(0);
+        ListNode* myPointer = &result;
+        
+        // carry variable
+        bool carry = 0;
+        
+        // while loop to add while there is digit or carry
+        while (l1 || l2 || carry) {
             
             // get the sum
-            sum = num1 + num2 + carry;
+            int sum = (l1 ? l1->val : 0) + (l2 ? l2->val : 0) + carry;
             
-            // store the digit then go to the next node (assumed carry for last sum)
-            out->next = new ListNode(1);
-            out = out->next;
-            out->val = sum % 10;
-
-            // store the carry
-            carry = (num1 + num2 + carry) / 10;
+            // link new node with the sum digit then move to next
+            myPointer->next = new ListNode(sum % 10);
+            myPointer = myPointer->next;
+            
+            // update the carry
+            carry = sum / 10;
+            
+            // move the ListNode if any
+            l1 = l1 ? l1->next : nullptr;
+            l2 = l2 ? l2->next : nullptr;
         }
         
-        if (carry) {
-            
-            out->next = new ListNode(1);
-        }
-        
-        return result;
+        // return the first digit of result
+        return result.next;
     }
 };
