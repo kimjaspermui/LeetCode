e98002ab30a7256ec9db5a98ed477fcab620b10b
Kim Jasper Mui <kmui@ucsd.edu>
2017-08-23 16:54:30 -0700

    Updated with much concise cpp solution

--- Longest Substring Without Repeating Characters/solution.cpp
+++ Longest Substring Without Repeating Characters/solution.cpp
@@ -1,51 +1,26 @@
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
-
-        // hash table to check character already seen along with its index
-        unordered_map<char, int> myMap;
+        
+        // array to check index of the current substring's characters
+        int myArray[256] = {0};
         
         // keep track of longest substring 
         int longest = 0;
         
-        // to keep track the beginning of current substring
-        int begin = 0;
-        int end = 0;
-        
         // for loop to iterate through the entire string
-        for (; end < s.length(); end++) {
-            
-            // case for non-repeated character
-            if (myMap.find(s[end]) == myMap.end()) {
-                
-                myMap.insert({s[end], end});
-            }
-            
-            // case for repeated character
-            else {
+        for (int begin = 0, end = 0; end < s.length(); end++) {
 
-                // update longest if necessary
-                int tempLength = end - begin;
-                longest = tempLength > longest ? tempLength : longest;
-                
-                // get the index of repeated chracter
-                int repeatedIndex = myMap.find(s[end])->second;
-                
-                // erase the ones before and including the repeated character
-                while (begin != repeatedIndex + 1) {
-                    
-                    myMap.erase(s[begin++]);
-                }
-                
-                // insert a new index for the repeated character
-                myMap.insert({s[end], end});
-            }
+            // update begin if the repeated character index is larger
+            begin = (myArray[s[end]] > begin) ? myArray[s[end]] : begin;
+            
+            // update the index of this character (all indices increases by 1)
+            myArray[s[end]] = end + 1;
+            
+            // update longest if necessary
+            longest = (end - begin + 1 > longest) ? end - begin + 1 : longest;
         }
 
-        // check the last substring
-        int lastLength = end - begin;
-        longest = lastLength > longest ? lastLength : longest;
-        
         return longest;
     }
 };
