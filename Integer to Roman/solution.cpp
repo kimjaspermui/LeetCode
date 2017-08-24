3ca5a31f29e3209e7cb7aeabe5158722859b084d
Kim Jasper Mui <kmui@ucsd.edu>
2017-08-24 10:03:18 -0700

    Made the cpp solution more concise by using array to separate the data

--- Integer to Roman/solution.cpp
+++ Integer to Roman/solution.cpp
@@ -2,31 +2,25 @@ class Solution {
 public:
     string intToRoman(int num) {
         
-        // this will hold all of the roman units
-        vector<pair<int, string>> romanUnits = {{1000, "M"}, {900, "CM"}, {500, "D"},
-                                                {400, "CD"}, {100, "C"}, {90, "XC"},
-                                                {50, "L"}, {40, "XL"}, {10, "X"},
-                                                {9, "IX"}, {5, "V"}, {4, "IV"},
-                                                {1, "I"}};
+        // these will hold all of the roman units along with its values
+        int values[] = {1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1};
+        string units[] = {"M", "CM", "D", "CD", "C", "XC", "L", "XL", "X", "IX", "V", "IV", "I"};
         
         // this will keep track the current roman unit pair from highest
         int unitIndex = 0;
         
         // this will hold the roman representation
-        string myString;
+        string myString = "";
         
         // while number is not 0
         while (num) {
             
-            // get the pair of units
-            pair<int, string> myPair = romanUnits[unitIndex];
-            
             // case when this unit is within the number
-            if (myPair.first <= num) {
+            if (values[unitIndex] <= num) {
                 
                 // append the unit then decrease the number by the value
-                myString.append(myPair.second);
-                num -= myPair.first;
+                myString += units[unitIndex];
+                num -= values[unitIndex];
             }
             
             // otherwise, go to the next unit
