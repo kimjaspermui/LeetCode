d61e003a030273fffee3d05cf11452a330cadc45
Kim Jasper Mui <kmui@ucsd.edu>
2017-09-09 16:32:51 -0700

    Improved cpp solution for First Unique Character in a String

--- First Unique Character in a String/solution.cpp
+++ First Unique Character in a String/solution.cpp
@@ -1,66 +1,29 @@
 class Solution {
 public:
     int firstUniqChar(string s) {
-        
-        // this map will store all of the characters have been seen so far
-        unordered_map<char, int> seenChar;
-        
-        // index initialized to -1
-        int nonRepeatIndex = -1;
 
-        // for loop to iterate all characters
+        // alphabet size and its count vector initialized to 0
+        int ALPHABET = 26;
+        vector<int> count(ALPHABET, 0);
+
+        // iterate all elements in the string to count
         for (int i = 0; i < s.length(); i++) {
             
-            // get the current character
-            char c = s[i];
-
-            // insert current index if not exist yet
-            if (seenChar.find(c) == seenChar.end()) {
-                
-                seenChar.insert({c, 0});
-            }
-            
             // increment the count for this character
-            seenChar.find(c)->second++;   
+            count[s[i] - 'a']++;
+        }
+        
+        // iterate all elements in the string to find the character with 1 count
+        for (int i = 0; i < s.length(); i++) {
             
-            // if index is -1, use this index if this character hasn't been repeated yet (count is 1)
-            if (nonRepeatIndex == -1) {
+            // count of 1 for this character
+            if (count[s[i] - 'a'] == 1) {
                 
-                if (seenChar.find(c)->second == 1) {
-
-                    nonRepeatIndex = i;
-                }
-            }
-            
-            // else if it's equal to the previous nonRepeated, then it's repeated
-            else if (c == s[nonRepeatIndex]) {
-
-                // increment and index
-                nonRepeatIndex++;
-                
-                // while loop until found a character that has only seen once
-                while (seenChar.find(s[nonRepeatIndex])->second != 1) {
-
-                    cout << "enter " << endl;
-                    // at the last index, return -1
-                    if (nonRepeatIndex == s.length() - 1) {
-                        
-                        return -1;
-                    }
-                    
-                    // at the current index, reset nonRepeatIndex then break
-                    else if (nonRepeatIndex == i) {
-                        
-                        nonRepeatIndex = -1;
-                        break;
-                    }
-                    
-                    // attempt to increment nonRepeatIndex
-                    nonRepeatIndex++;
-                }
+                return i;
             }
         }
         
-        return nonRepeatIndex;
+        // otherwise, just return -1 for no non-repeat character
+        return -1;
     }
 };
