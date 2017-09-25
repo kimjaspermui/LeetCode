556f0a8a41227e6427e61aa76869d66e55f18468
Kim Jasper Mui <kmui@ucsd.edu>
2017-09-25 11:19:08 -0800

    Redid One Edit Distance

---
+++ One Edit Distance 2/solution.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    bool isOneEditDistance(string s, string t) {
+
+        for (int i = 0; i < min(s.size(), t.size()); i++) {
+
+            if (s[i] != t[i]) {
+
+                if (s.size() == t.size()) {
+
+                    return s.substr(i+1, string::npos) == t.substr(i+1, string::npos);
+                }
+                else if (s.size() < t.size()) {
+
+                    return s.substr(i, string::npos) == t.substr(i+1, string::npos);
+                }
+
+                else {
+
+                    return s.substr(i+1, string::npos) == t.substr(i, string::npos);
+                }
+            }
+        }
+    }
+
+    return abs(int(s.size()) - int(t.size())) == 1;
+};
