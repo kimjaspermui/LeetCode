1faf648ae16a7dcc69a34ecfad2d7344c78d6e46
Kim Jasper Mui <kmui@ucsd.edu>
2017-10-07 19:55:37 -0700

    Updated Reverse String cpp solution with more efficient algorithm

--- Reverse String/solution.cpp
+++ Reverse String/solution.cpp
@@ -1,23 +1,16 @@
 class Solution {
 public:
     string reverseString(string s) {
-
-        // initialized space for the return string
-        char myCharArray[s.length() + 1];
-
-        // get the length of the string
-        int myLength = s.length();
-
-        // for loop to iterate the entire string
-        for (int i = 0; i < myLength; i++) {
-
-            // get characters from the end
-            myCharArray[i] = s[myLength - i - 1];
+        
+        // loop through the string until i is >= j, othewise, swap the characters
+        for (int i = 0, j = s.length() - 1; i < j; i++, j--) {
+            
+            // swap the characters
+            int temp = s[i];
+            s[i] = s[j];
+            s[j] = temp;
         }
-
-        // add a terminated character
-        myCharArray[myLength] = '\0';
-
-        return myCharArray;
+        
+        return s;
     }
 };
