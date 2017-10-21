eb2f9cfbd39e87fbd5257e00e6e9802e359cd344
Kim Jasper Mui <kmui@ucsd.edu>
2017-10-21 06:07:22 -0700

    Practice Longest Palindromic Substring again

--- Longest Palindromic Substring/Solution.cpp
+++ Longest Palindromic Substring/Solution.cpp
@@ -1,46 +1,40 @@
 class Solution {
 public:
     
-    // this will check a potential palindrome
-    void checkPalindrome(string s, int j, int k, int& start, int& maxLength) {
+    void checkPalindrome(int low, int high, string s, int& start, int& maxLength) {
         
-        // while it's within the bound check move two sides
-        while (j >= 0 && k < s.size() && s[j] == s[k]) {
+        // while in bound and equal, decrement and increment
+        while (low >= 0 && high < s.length() && s[low] == s[high]) {
             
-            j--;
-            k++;
+            low--;
+            high++;
         }
         
-        // compare the current palindrome size with the maximum length
-        if (maxLength < k - j - 1) {
+        // get the current length
+        int tempLength = high - low - 2 + 1;
+        
+        // if greater than max, then update low index and the max length
+        if (tempLength > maxLength) {
             
-            // update the length if it's greater, also update the starting index
-            maxLength = k - j - 1;
-            start = j + 1;
+            start = low + 1;
+            maxLength = tempLength;
         }
     }
     
     string longestPalindrome(string s) {
-
-        // just return the string if 0 or 1 length
-        if (s.size() < 2) {
-
-            return s;
-        }
-
-        // this will keep track of the low index of longest palindrome and its length
+        
+        // initialize the start and the maximum length
         int start = 0;
-        int maxLength = 1;
-
+        int maxLength = 0;
+        
+        // for loop to start from all characters
         for (int i = 0; i < s.length(); i++) {
-
-            // check odd length palindrome
-            checkPalindrome(s, i, i, start, maxLength);
             
-            // check even length palindrome
-            checkPalindrome(s, i, i + 1, start, maxLength);
+            // odd and even case
+            checkPalindrome(i, i, s, start, maxLength);
+            checkPalindrome(i, i+1, s, start, maxLength);
         }
-
+        
         return s.substr(start, maxLength);
     }
 };
