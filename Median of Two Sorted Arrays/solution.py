9b647f539de120cbbf6a37334eaf4009445755bd
Kim Jasper Mui <kmui@ucsd.edu>
2017-12-18 10:35:46 -0800

    Added empty cases and made non-overlap cases more robust using median helper function in Median of Two Sorted Arrays

--- Median of Two Sorted Arrays/solution.py
+++ Median of Two Sorted Arrays/solution.py
@@ -1,44 +1,38 @@
 class Solution:
+    def median(self, l, length):
+        '''this helper function will calculate the median of a list'''
+
+        middle = length / 2
+
+        # even case, get the average
+        if length % 2 == 0:
+            return (l[middle] + l[middle-1]) / 2.0
+
+        # odd case, get the middle
+        else:
+            return l[middle]
+
     def noOverlap(self, l1, l2):
-        '''this function will return the median of non-overlap lists'''
+        '''this helper function will return the median of non-overlap lists'''
         '''l1's elements are smaller than l2's'''
 
-        # even case
-        if (len(l1) + len(l2)) % 2 == 0:
+        # even and odd cases combine, don't need to separate them, just need
+        # to pass in correct length and list to median helper function
 
-            # length of l1 is less than l2, then take length of l1 from
-            # l2 then divide by 2 to get the larger middle index in l2
-            # taking the average of it with lesser index element to get
-            # the median
-            if len(l1) < len(l2):
-                middle = (len(l2) - len(l1)) / 2
-                return (l2[middle] + l2[middle-1]) / 2
+        # length of l1 is less than l2, take length of l1 from l2 to get
+        # the length to calculate median using the helper function
+        if len(l1) < len(l2):
+            return median(l2, len(l2) - len(l1))
 
-            # length of l2 is less than l1, similar to above but get
-            # the total length to get the middle index
-            elif len(l1) > len(l2):
-                middle = (len(l2) + len(l1)) / 2
-                return (l1[middle] + l1[middle-1]) / 2
+        # length of l2 is less than l1, add length of l2 to l1 to get
+        # the length to calculate median using the helper function
+        elif len(l1) > len(l2):
+            return median(l1, len(l2) + len(l1))
 
-            # equal case, returns the sum of end of l1 and begin of l2
-            # divide by 2
-            else:
-                return (l1[-1] + l2[0]) / 2.0
-
-        # odd case
+        # equal case (only for even), returns the sum of end of l1 and begin
+        # of l2 divide by 2
         else:
-
-            # length of l1 is less than l2, then take length of l1 from
-            # l2 then divide by 2 to get the index of median in l2
-            if len(l1) < len(l2):
-                return l2[(len(l2) - len(l1)) / 2]
-
-            # length of l2 is less than l1, then add length of l2 to
-            # l1 then divide by 2 to get the index of median in l1
-            elif len(l1) > len(l2):
-                 return l1[(len(l1) + len(l2)) / 2]
-
-            # there won't be equal case
+            return (l1[-1] + l2[0]) / 2.0
 
     def findMedianSortedArrays(self, nums1, nums2):
         """
@@ -46,15 +40,26 @@ class Solution:
         :type nums2: List[int]
         :rtype: float
         """
+
+        # both are empty, then return 0.0
+        if nums1 == [] and nums2 == []:
+            return 0.0
+
+        # num1 is empty then get the median from num2
+        if nums1 == []:
+            return median(nums2, len(nums2))
+
+        # num2 is empty then get the median from num1
+        if nums2 == []:
+            return median(nums1, len(nums2))
+
         # non-overlap case, first list has smaller elements
-        if num1[-1] <= num2[0]:
-            return self.noOverlap(num1, num2)
+        if nums1[-1] <= nums2[0]:
+            return self.noOverlap(nums1, nums2)
 
         # non-overlap case, second list has smaller elements
-        elif num2[-1] <= num1[0]:
-            return self.noOverlap(num2, num1)
+        if nums2[-1] <= nums1[0]:
+            return self.noOverlap(nums2, nums1)
 
         # overlap case (hard)
-        else:
-
-            
+        #else:
