d539c79ce830dbb1ce6b77b28277332602c848e9
Kim Jasper Mui <kmui@ucsd.edu>
2017-12-19 14:08:15 -0800

    Added Javascript to language selector

--- languageSelector.py
+++ languageSelector.py
@@ -1,5 +1,5 @@
 from random import *
 
-myLanguage = ["C++", "Java", "Python", "Python3", "C", "Swift", "C#"]
+myLanguage = ["C++", "Java", "Python", "Python3", "C", "Swift", "C#", "Javascript"]
 
 print("Let's use %s" % (myLanguage[randint(0, len(myLanguage)-1)]))
