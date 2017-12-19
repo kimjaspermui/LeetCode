924cacfd7cb7ad1a7526a3f7a0f2def81c8c54c8
Kim Jasper Mui <kmui@ucsd.edu>
2017-12-18 20:55:55 -0800

    Added Python3 solution for Longest Palindromic Substring

--- Longest Palindromic Substring/Solution.py
+++ Longest Palindromic Substring/Solution.py
@@ -1,22 +1,37 @@
-def checkPalindrome(low, high, s, start, maxLength):
-    while low >= 0 and high < len(s) and s[low] == s[high]:
-        low -= 1
-        high += 1
-    tempLength = high - low - 2 + 1
-    if tempLength >= maxLength:
-        return low + 1, tempLength
-    else:
-        return start, maxLength
+class Solution:
+    def checkPalindrome(self, s, i, j, longest, start):
+        '''this helper function will check the longest palindrome starting'''
+        '''from this middle'''
 
-class Solution(object):  
+        # loop until not equal or out of bounds
+        while i >= 0 and j < len(s) and s[i] == s[j]:
+            i -= 1
+            j += 1
+
+        # get their difference, then minus 1 for one of the sides
+        tempLength = j - i -1
+
+        # if it is larger, then return updated values
+        if tempLength > longest:
+            return tempLength, i+1
+
+        # otherwise, return the current values
+        else:
+            return longest, start
+
     def longestPalindrome(self, s):
         """
         :type s: str
         :rtype: str
         """
+
+        # this will keep track of the start index of the longest substring and length
         start = 0
-        maxLength = 0
-        for i in range(0, len(s)):
-            start, maxLength = checkPalindrome(i, i, s, start, maxLength)
-            start, maxLength = checkPalindrome(i, i + 1, s, start, maxLength)
-        return s[start:start + maxLength]
+        longest = 0
+
+        # for loop to iterate all indices starting from the second index
+        for i in range(len(s)):
+            longest, start = self.checkPalindrome(s, i, i, longest, start)
+            longest, start = self.checkPalindrome(s, i, i+1, longest, start)
+
+        return s[start:start+longest]
