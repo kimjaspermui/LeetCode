5ed26d79c90b99e0d2530d2d19f17505621570ae
Kim Jasper Mui <kmui@ucsd.edu>
2017-12-20 09:59:09 -0800

    Removed C in language selector

--- languageSelector.py
+++ languageSelector.py
@@ -1,5 +1,5 @@
 from random import *
 
-myLanguage = ["C++", "Java", "Python", "Python3", "C", "Swift", "C#", "Javascript"]
+myLanguage = ["C++", "Java", "Python", "Python3", "Swift", "C#", "Javascript"]
 
 print("Let's use %s" % (myLanguage[randint(0, len(myLanguage)-1)]))
