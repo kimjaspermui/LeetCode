5d335c2be30a7bcca7fdc9db438478b450f3b9bd
Kim Jasper Mui <kmui@ucsd.edu>
2018-02-04 23:22:34 -0800

    Added new cpp solution for Merge k Sorted Lists, Leetcode is down, so it hasn't been tested

--- Merge k Sorted Lists/solution.cpp
+++ Merge k Sorted Lists/solution.cpp
@@ -70,3 +70,69 @@ public:
         return dummyHead.next;
     }
 };
+
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeKLists(vector<ListNode*>& lists) {
+        
+        // initialize output list head
+        ListNode dummyHead(-1);
+        ListNode* current = &dummyHead;
+        
+        // comparison function for priority queue
+        auto myComp = [](const ListNode* a, const ListNode* b) {
+            
+            if (a->val >= b->val) {
+                
+                return true;
+            }
+            
+            return false;
+        }
+        
+        // define priority queue
+        priority_queue<ListNode*>, vector<ListNode*>, myComp> myPQ;
+        
+        // insert head of all lists
+        for (int i = 0; i < lists.size(); i++) {
+            
+            // don't insert nullptr
+            if (lists[i] != nullptr) {
+                
+                myPQ.push(lists[i]);
+            }
+        }
+        
+        // keep looping until priority queue is empty
+        while (!myPQ.empty()) {
+            
+            // get the min node
+            ListNode* minNode = myPQ.top();
+            myPQ.pop();
+            
+            // update the pointers
+            current->next = minNode;
+            current = current->next;
+            
+            // get the next of minNode
+            ListNode* next = minNode->next;
+            
+            // if it is not nullptr, then push it to pq
+            if (next != nullptr) {
+                
+                myPQ.push(next);
+            }
+        }
+        
+        return dummyHead.next;
+    }
+};
